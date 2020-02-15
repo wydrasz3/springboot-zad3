79e61d125e4acc791e3e9d071098dbfa58b48b48
MSI <wydrasz3@gmail.com>
2020-02-15 10:45:30 +0100

    test

--- src/test/java/pl/sda/zad3/calculator/WorkingDayCalculatorTest.java
+++ src/test/java/pl/sda/zad3/calculator/WorkingDayCalculatorTest.java
@@ -1,23 +1,32 @@
 package pl.sda.zad3.calculator;
 
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.Date;
-
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
 
 class WorkingDayCalculatorTest {
 
-    @Test
-    public void testWorkingDay() throws ParseException {
+    @ParameterizedTest
+    @MethodSource("provideData")
+    public void testWorkingDay(String date, boolean  expectedResult) throws ParseException {
         SimpleDateFormat dateFormat = new SimpleDateFormat("dd-MM-yyyy");
-        Date workingDay = dateFormat.parse("14-02-2020");
+        Date workingDay = dateFormat.parse(date);
         WorkingDayCalculator calculator = new WorkingDayCalculator();
         boolean isWorkingDate = calculator.check(workingDay);
-        Assertions.assertTrue(isWorkingDate);
+        Assertions.assertEquals(isWorkingDate, expectedResult);
+    }
+
+    private static Stream<Arguments> provideData() {
+        return Stream.of(
+                Arguments.of("14-02-2020", true),
+                Arguments.of("15-02-2020", false)
+        );
     }
 
 }
